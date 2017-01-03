4443d081597f47281c8fe909068b0144ea47e74e
AMU\vb023528 <richard_che-so@ch.honda.com>
2017-01-03 13:42:07 -0500

    Add Loop to Btree Tick Coroutine

--- Assets/TestBtree.cs
+++ Assets/TestBtree.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using FluentBehaviourTree;
 using UnityEngine;
 using System.Threading;
+using System.Collections;
 
 public class TestBtree : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class TestBtree : MonoBehaviour
     IBehaviourTreeNode btree1;
     Dictionary<string, string> callData;
     private bool done = false;
+    private bool continueBehaviourTree = true;
+    private int maxloops = 5;
+    private int numloops = 1;
 
     string seq1Action1 = "Sequence1Action1";
     string seq1Action2 = "Sequence1Action2";
@@ -39,26 +43,50 @@ public class TestBtree : MonoBehaviour
         Init();
         Log("Before StartCoroutine()");
         StartCoroutine(TickTree("Some Input Value", (myReturnValue, location) => {
-            Log("Returned Value from " + location + " is: " + myReturnValue);
+            Log("Callback invoked in Start-Log() - Returned Value from " + location + " is: " + myReturnValue);
         }));
         Log("After StartCoroutine()");
     }
 
-    IEnumerator<BehaviourTreeStatus> TickTree(string inputval, System.Action<BehaviourTreeStatus, string> callback)
+    IEnumerator TickTree(string inputval, System.Action<BehaviourTreeStatus, string> callback)
     {
-        Log("Beginning of TickTree()");
-        for (var e = btree1.Tick(new TimeData(Time.deltaTime)); e.MoveNext();)
+        while (continueBehaviourTree)
         {
-            yield return e.Current;
-            callback(e.Current, "Callback From TickTree()");
+            done = false;
+            Log("\n\n");
+            callData = new Dictionary<string, string>();
+            Log("============> Beginning of TickTree(): input Value=" + inputval + " ---> Loop Number: " + numloops);
+           
+            int numticks = 0;
+            for (var e = btree1.Tick(new TimeData(Time.deltaTime)); e.MoveNext();)
+            {
+                ++numticks;
+                yield return e.Current;
+                callback(e.Current, "Callback Called From TickTree() - Tick Number=" + numticks + " --> Behavior Status=" + e.Current);
+            }
+
+            Log("End of TickTree() - Done=true");
+            done = true;
+
+            Log("--> Tree Hierarchy after:");
+            Log(btree1.getTreeAsString(" --> "));
+
+            Log("--> Tree Hierarchy Reset:");
+            btree1.SetStatusAll(BehaviourTreeStatus.Initial);
+            Log(btree1.getTreeAsString(" --> "));
+
+            Debug.Log(report);
+            report = "";
+            ++numloops;
+            if (numloops > maxloops)
+            {
+                continueBehaviourTree = false;
+                break;
+            }
+            yield return new WaitForSeconds(2);
         }
-       
-        Log("End of TickTree() - Done=true");
-        done = true;
-        
-        Log("--> Tree Hierarchy after:");
-        Log(btree1.getTreeAsString(" --> "));
-        Debug.Log(report);
+        yield return null;
+      
     }
 
     void Update()
@@ -144,12 +172,13 @@ public class TestBtree : MonoBehaviour
     }
     public IEnumerator<BehaviourTreeStatus> actionSuccess(TimeData t, string aValue)
     {
-       Log(aValue + " --> Action Successful ! at Delta time:" + t.deltaTime);
+        Log(aValue + " --> Action Successful ! at Delta time:" + t.deltaTime);
         callData.Add(aValue + t.deltaTime, FactionSuccess);
-       Log("Return Status Running from actionSuccess ! - coroutine will return and then restart after this point.");
+        Log("Return Status Running from actionSuccess ! - coroutine will return and then restart after this point.");
         yield return BehaviourTreeStatus.Running;
-       // int milliseconds = 100;
-       // Thread.Sleep(milliseconds);
+        // int milliseconds = 100;
+        // Thread.Sleep(milliseconds);
+        Log("coroutine continuing in actionSuccess.. will return Success in this tick -");
         yield return BehaviourTreeStatus.Success;
     }
     public IEnumerator<BehaviourTreeStatus> actionFail(TimeData t, string aValue)
@@ -157,10 +186,11 @@ public class TestBtree : MonoBehaviour
        Log(aValue + " --> Action Failed ! at Delta time:" + t.deltaTime);
         //throw new ApplicationException("Node Failure to Execute !!");
         callData.Add(aValue + t.deltaTime, FactionFail);
-       Log("Return Status Running from actionFail ! - coroutine will return and then restart after this point.");
+        Log("Return Status Running from actionFail ! - coroutine will return and then restart after this point.");
         yield return BehaviourTreeStatus.Running;
-       // int milliseconds = 100;
-       // Thread.Sleep(milliseconds);
+        // int milliseconds = 100;
+        // Thread.Sleep(milliseconds);
+        Log("coroutine continuing in actionFail.. will return Failure in this tick -");
         yield return BehaviourTreeStatus.Failure;
     }
 
